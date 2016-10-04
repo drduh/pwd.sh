b2ac78109207b526de29184a26ce42c5136e5243
Juergen Hoetzel <juergen@archlinux.org>
2016-10-04 18:00:00 +0200

    Don't evaluate \ when reading username
    This allows entering Microsoft Domain users like DOMAIN\USER.

--- pwd.sh
+++ pwd.sh
@@ -74,7 +74,7 @@ read_pass () {
   fi
 
   if [[ -z "${2+x}" ]] ; then
-    read -p "
+    read -r -p "
   Username to read? (default: all) " username
   else
     username="${2}"
@@ -146,7 +146,7 @@ create_username () {
   # Create username with password.
 
   if [[ -z "${2+x}" ]] ; then
-    read -p "
+    read -r -p "
   Username: " username
   else
     username="${2}"
