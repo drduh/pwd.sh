4944480937fcb73551ceaf2f6b67b299ccf6356f
drduh <github@duh.to>
2019-11-26 10:49:18 -0800

    Version 2 beta, see release notes in README.

--- README.md
+++ README.md
@@ -1,59 +1,78 @@
-# pwd.sh
+pwd.sh is a Bash shell script to manage passwords and other secrets.
 
-Script to manage passwords in an encrypted file using gpg.
+It uses GnuPG to symmetrically (i.e., using a master password) encrypt and decrypt plain text files.
 
-![screencast gif](https://i.imgur.com/sQoF3VN.gif)
+[drduh/Purse](https://github.com/drduh/Purse) is a fork which uses public key authentication instead of a master password and can integrate with YubiKey.
 
-**New!** [drduh/Purse](https://github.com/drduh/Purse) is a fork which uses public key authentication instead of a master passphrase and can integrate with YubiKey.
+# Release notes
 
-# Installation
+## Version 1 (2015)
+
+The original release which has been available for general use and review since July 2015. There are no known bugs nor security vulnerabilities identified in this stable version of pwd.sh. Compatible on Linux, OpenBSD, macOS.
+
+## Version 2b (2019)
+
+The second release of pwd.sh features several security and reliability improvements, and is an optional upgrade. Currently in beta testing. Compatible on Linux, OpenBSD, macOS.
+
+Changelist:
+* Passwords are now encrypted as individual files, rather than all encrypted as a single flat file.
+* Individual password filenames are random, mapped to usernames in an encrypted index file.
+* Index and password files are now "immutable" using chmod while pwd.sh is not running.
+* Read passwords are now copied to clipboard and cleared after a timeout, instead of printed to stdout.
+* Use printf instead of echo for improved portability.
+* New option: list passwords in the index.
+* New option: create tar archive for backup.
+* Removed option: delete password; the index is now a permanent ledger.
+* Removed option: read all passwords; no use case for having a single command.
+* Removed option: suppress generated password output; should be read from safe to verify save.
+
+# Use
 
 ```console
 $ git clone https://github.com/drduh/pwd.sh
 ```
 
-# Use
+`cd pwd.sh` and run the script interactively using `./pwd.sh` or symlink to a directory in `PATH`:
 
-`cd pwd.sh` and run the script interactively using `./pwd.sh`
-
-* Type `w` to write a password.
-* Type `r` to read a password.
-* Type `d` to delete a password.
-* Type `h` to print the help text.
+* Type `w` to write a password
+* Type `r` to read a password
+* Type `l` to list passwords
+* Type `b` to create an archive for backup
+* Type `h` to print the help text
 
 Options can also be passed on the command line.
 
-Examples:
+Example usage:
 
-Create 30-character password for `gmail`:
-
-```console
-$ ./pwd.sh w gmail 30
-```
-
-Append `q` to create a password without displaying it.
-
-Read password for `user@github`:
+Create a 30-character password for `userName`:
 
 ```console
-$ ./pwd.sh r user@github
+$ ./pwd.sh w userName 30
 ```
 
-Delete password for `dropbox`:
+Read password for `userName`:
 
 ```console
-$ ./pwd.sh d dropbox
+$ ./pwd.sh r userName
 ```
 
-Copy password for `github` to clipboard (substitute `pbcopy` on macOS):
+Passwords are stored with a timestamp for revision control. The most recent version is copied to clipboard on read. To list all passwords or read a previous version of a password:
 
 ```console
-$ ./pwd.sh r github | cut -f 1 -d ' ' | awk 'NR==3{print $1}' | xclip
+$ ./pwd.sh l
+
+$ ./pwd.sh r userName@1574723600
 ```
 
-The script and encrypted `pwd.sh.safe` file can be publicly shared between trusted computers.
+Create an archive for backup:
 
-See [drduh/config/gpg.conf](https://github.com/drduh/config/blob/master/gpg.conf) for additional GPG options.
+```console
+$ ./pwd.sh b
+```
+
+The backup contains only encrypted files and can be publicly shared for use on trusted computers.
+
+See [drduh/config/gpg.conf](https://github.com/drduh/config/blob/master/gpg.conf) for additional GPG configuration options.
 
 # Similar software
 
--- pwd.sh
+++ pwd.sh
@@ -5,21 +5,24 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+#set -x # uncomment to debug
+
 umask 077
 
-filter="$(command -v grep) -v -E"
+now=$(date +%s)
+copy="$(command -v xclip || command -v pbcopy)"
 gpg="$(command -v gpg || command -v gpg2)"
-safe="${PWDSH_SAFE:=pwd.sh.safe}"
-
+backuptar="${PWDSH_BACKUP:=pwd.sh.backup.$(date +%F).tar}"
+safeix="${PWDSH_INDEX:=pwd.sh.index}"
+safedir="${PWDSH_SAFE:=safe}"
+timeout=9
 
 fail () {
   # Print an error message and exit.
 
-  printf "\n\n"
-  tput setaf 1 1 1 ; echo "Error: ${1}" ; tput sgr0
+  tput setaf 1 1 1 ; printf "\nError: ${1}\n" ; tput sgr0
   exit 1
 }
-
 
 get_pass () {
   # Prompt for a password.
@@ -44,187 +47,199 @@ get_pass () {
   done
 }
 
-
 decrypt () {
-  # Decrypt with a password.
+  # Decrypt with GPG.
 
-  echo "${1}" | ${gpg} --armor --batch --no-symkey-cache \
+  printf '%s\n' "${1}" | ${gpg} --armor --batch --no-symkey-cache \
     --decrypt --passphrase-fd 0 "${2}" 2>/dev/null
 }
 
-
 encrypt () {
-  # Encrypt with a password.
+  # Encrypt with GPG.
 
   ${gpg} --armor --batch \
     --symmetric --yes --passphrase-fd 3 --no-symkey-cache \
-    --output "${2}" "${3}" 3< <(echo "${1}")
+    --output "${2}" "${3}" 3< <(printf '%s\n' "${1}") 2>/dev/null
 }
-
 
 read_pass () {
   # Read a password from safe.
 
-  if [[ ! -s ${safe} ]] ; then fail "${safe} not found" ; fi
+  if [[ ! -s ${safeix} ]] ; then fail "${safeix} not found" ; fi
 
-  if [[ -z "${2+x}" ]] ; then read -r -p "
-  Username (Enter for all): " username
-  else
-    username="${2}"
-  fi
-
-  if [[ -z "${username}" || "${username}" == "all" ]] ; then username="" ; fi
+  username=""
+  while [[ -z "${username}" ]] ; do
+    if [[ -z "${2+x}" ]] ; then read -r -p "
+  Username: " username
+    else username="${2}" ; fi
+  done
 
   while [[ -z "${password}" ]] ; do get_pass "
-  Password to unlock ${safe}: " ; done
-  printf "\n\n"
+  Password to unlock ${safeix}: " ; done
+  printf "\n"
 
-  decrypt "${password}" ${safe} | grep -F " ${username}" \
-    || fail "Decryption failed"
+  spath=$(decrypt ${password} ${safeix} | \
+    grep -F "${username}" | tail -n1 | cut -d : -f2) || \
+      fail "Failed to decrypt ${safeix}"
+
+  clip <(decrypt ${password} ${spath}) || \
+    fail "Failed to decrypt ${spath}"
 }
 
-
 gen_pass () {
-  # Generate a password.
+  # Generate a password using GPG.
 
   len=20
   max=80
 
   if [[ -z "${3+x}" ]] ; then read -p "
-
   Password length (default: ${len}, max: ${max}): " length
-  else
-    length="${3}"
-  fi
+  else length="${3}" ; fi
 
   if [[ ${length} =~ ^[0-9]+$ ]] ; then len=${length} ; fi
 
   # base64: 4 characters for every 3 bytes
   ${gpg} --armor --gen-random 0 "$((${max} * 3/4))" | cut -c -"${len}"
- }
-
-
-write_pass () {
-  # Write a password in safe.
-
-  # If no password (delete action), clear the entry by writing an empty line.
-  if [[ -z "${userpass+x}" ]] ; then
-    entry=" "
-  else
-    entry="${userpass} ${username}"
-  fi
-
-  get_pass "
-  Password to unlock ${safe}: " ; echo
-
-  # If safe exists, decrypt it and filter out username, or bail on error.
-  # If successful, append entry, or blank line.
-  # Filter blank lines and previous timestamp, append fresh timestamp.
-  # Finally, encrypt it all to a new safe file, or fail.
-  # If successful, update to new safe file.
-  ( if [[ -f "${safe}" ]] ; then
-      decrypt "${password}" ${safe} | \
-      ${filter} " ${username}$" || return
-    fi ; \
-    echo "${entry}") | \
-    (${filter} "^[[:space:]]*$|^mtime:[[:digit:]]+$";echo mtime:$(date +%s)) | \
-    encrypt "${password}" ${safe}.new - || fail "Write to safe failed"
-    mv ${safe}{.new,}
 }
 
+write_pass () {
+  # Write a password and update index file.
+
+  get_pass "
+  Password to unlock ${safeix}: "
+  printf "\n"
+
+  fpath=$(tr -dc "[:lower:]" < /dev/urandom | fold -w8 | head -n1)
+  spath=${safedir}/${fpath}
+  printf '%s\n' "${userpass}" | \
+    encrypt "${password}" ${spath} - || \
+      fail "Failed to put ${spath}"
+
+  ( if [[ -f "${safeix}" ]] ; then
+      decrypt "${password}" ${safeix} || return ; fi
+    printf "${username}@${now}:${spath}\n") | \
+    encrypt "${password}" ${safeix}.${now} - || \
+      fail "Failed to put ${safeix}.${now}"
+  mv ${safeix}{.${now},}
+}
+
+list_entry () {
+  # Decrypt the index to list entries.
+
+  if [[ ! -s ${safeix} ]] ; then fail "${safeix} not found" ; fi
+
+  while [[ -z "${password}" ]] ; do get_pass "
+  Password to unlock ${safeix}: " ; done
+  printf "\n\n"
+
+  decrypt ${password} ${safeix} || fail "Decryption failed"
+}
+
+backup () {
+  # Archive encrypted index and safe directory.
+
+  if [[ -f ${safeix} && -d ${safedir} ]] ; then \
+    tar cfv ${backuptar} ${safeix} ${safedir}
+  else fail "Nothing to archive" ; fi
+  printf "\nArchived ${backuptar}\n" ; \
+}
+
+clip () {
+  # Use clipboard and clear after timeout.
+
+  ${copy} < ${1}
+
+  printf "\n"
+  shift
+  while [ $timeout -gt 0 ] ; do
+    printf "\r\033[KPassword on clipboard! Clearing in %.d" $((timeout--))
+    sleep 1
+  done
+
+  printf "" | ${copy}
+}
 
 new_entry () {
   # Prompt for new username and/or password.
 
-  if [[ -z "${2+x}" ]] ; then read -r -p "
+  username=""
+  while [[ -z "${username}" ]] ; do
+    if [[ -z "${2+x}" ]] ; then read -r -p "
   Username: " username
-  else
-    username="${2}"
-  fi
+    else username="${2}" ; fi
+  done
 
   if [[ -z "${3+x}" ]] ; then get_pass "
   Password for \"${username}\" (Enter to generate): "
     userpass="${password}"
   fi
 
-  if [[ -z "${password}" ]] ; then userpass=$(gen_pass "$@")
-    if [[ -z "${4+x}" || ! "${4}" =~ ^([qQ])$ ]] ; then
-      echo "
-  Password: ${userpass}"
-    fi
-  fi
+  printf "\n"
+  if [[ -z "${password}" ]] ; then userpass=$(gen_pass "$@") ; fi
 }
 
 print_help () {
   # Print help text.
 
-  echo "
-  pwd.sh is a shell script to manage passwords with GnuPG symmetric encryption.
+  printf """
+  pwd.sh is a Bash shell script to manage passwords with GnuPG symmetric encryption.
 
-  pwd.sh can be used interactively as or with one of the following options:
+  pwd.sh can be used interactively or by passing one of the following options:
 
-    * 'r' to read a password
     * 'w' to write a password
-    * 'd' to delete a password
-    * 'h' to print this help text
+    * 'r' to read a password
+    * 'l' to list passwords
+    * 'b' to create an archive for backup
 
-  A username, password length and 'q' options can also be used.
+  Example usage:
 
-  Examples:
+    * Generate a 30 character password for 'userName':
+        ./pwd.sh w userName 30
 
-    * Read all passwords:
+    * Copy the password for 'userName' to clipboard:
+        ./pwd.sh r userName
 
-      ./pwd.sh r all
+    * List stored passwords and copy a previous version:
+        ./pwd.sh l
+        ./pwd.sh r userName@1574723625
 
-    * Write a password for 'github':
-
-      ./pwd.sh w github
-
-    * Generate a 50 character password for 'github' and write:
-
-      ./pwd.sh w github 50
-
-    * Generate a password and write without displaying it:
-
-      ./pwd.sh w github 50 q
-
-    * Delete password for 'mail':
-
-      ./pwd.sh d mail
-
-  A password cannot be supplied as an argument, nor is used as one in the script, to prevent it from appearing in process listing or logs."
+    * Create and restore a backup:
+        ./pwd.sh b
+        tar xvf pwd.sh.backup.*.tar"""
 }
-
 
 if [[ -z ${gpg} && ! -x ${gpg} ]] ; then fail "GnuPG is not available" ; fi
 
+if [[ ! -d ${safedir} ]] ; then mkdir -p ${safedir} ; fi
+
+chmod -R 0600 ${safeix}  2>/dev/null
+chmod -R 0700 ${safedir} 2>/dev/null
+
 password=""
-
 action=""
-if [[ -n "${1+x}" ]] ; then
-  action="${1}"
-fi 
+if [[ -n "${1+x}" ]] ; then action="${1}" ; fi
 
-while [[ -z "${action}" ]] ;
-  do read -n 1 -p "
-  Read, Write, or Delete password (or Help): " action
+while [[ -z "${action}" ]] ; do
+  read -n 1 -p "
+  Read or Write (or Help for more options): " action
   printf "\n"
 done
 
 if [[ "${action}" =~ ^([hH])$ ]] ; then
   print_help
+
+elif [[ "${action}" =~ ^([bB])$ ]] ; then
+  backup
+
+elif [[ "${action}" =~ ^([lL])$ ]] ; then
+  list_entry
+
 elif [[ "${action}" =~ ^([wW])$ ]] ; then
   new_entry "$@"
   write_pass
-elif [[ "${action}" =~ ^([dD])$ ]] ; then
-  if [[ -z "${2+x}" ]] ; then read -p "
-  Username: " username
-  else
-    username="${2}"
-  fi
-  write_pass
-else
-  read_pass "$@"
-fi
 
-printf "\n" ; tput setaf 2 2 2 ; echo "Done" ; tput sgr0
+else read_pass "$@" ; fi
+
+chmod -R 0400 ${safeix} ${safedir} 2>/dev/null
+
+tput setaf 2 2 2 ; printf "\nDone\n" ; tput sgr0
